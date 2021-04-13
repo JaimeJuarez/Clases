c4cb03a445b74875998ebe8653c79012b2ff7836
JaimeJuarez <jjuarez17@ucol.mx>
2021-04-13 13:48:18 -0500

    Primer intento

---
+++ Inventario.js
@@ -0,0 +1,5 @@
+var array = new array(4)
+array[0] = document.getElementById('Nombre')
+array[1] = document.getElementById('Precio')
+array[2] = document.getElementById('Cantidad')
+array[3] = document.getElementById('Numero')
